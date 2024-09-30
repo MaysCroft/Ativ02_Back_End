6bb6663931dcde644694b759a652f62d16db76af
MaysCroft <mayconpilar19@gmail.com>
2024-09-30 19:14:47 -0300

    Atividade 02 - Utilizando API's com Javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Script Site Adivinhação
 
-const apiKey = 'd24459e58cb14678969f264440d354e9'
+const apiKey = 'd24459e58cb14678969f264440d354e9' // Chave API para acessar o conteúdo do jogo
 
 let correctGame;
 
@@ -33,7 +33,7 @@ function adivinharGames() {
     .catch(error => console.error('Erro', error));
 };
 
-function pergunta() {
+function pergunta() { // Função 
 
     document.getElementById('pergunta').innerHTML = '<h3> Qual é o Jogo? </h3>';
     document.getElementById('jogo_img').src = correctGame.background_image;
@@ -51,14 +51,18 @@ function pergunta() {
     });
 };
 
-function checkAnswer(selected) {
+function checkAnswer(selected) { // Função para checar se a opção selecionada está correta
 
     const resultDiv = document.getElementById('result');
     
     if (selected.id === correctGame.id) {
-        resultDiv.innerHTML = '<h4> CORRETO!!! </h4>';
+        resultDiv.innerHTML = `<h4 style="color:#32CD32"> CORRETO!!! </h4> 
+                               <br/> 
+                               <img src="img/pinkiepie1.gif" alt="pinkie" class="pinkie">`;
     } else {
-        resultDiv.innerHTML = `<h4> INCORRETO!!! O jogo era: ${correctGame.name} </h4>`;
+        resultDiv.innerHTML = `<h4 style="color:#FF0000"> INCORRETO!!! O jogo era: ${correctGame.name} </h4> 
+                               <br/> 
+                               <img src="img/pinkiepie2.gif" alt="pinkie" class="pinkie">`;
     }
 
     document.getElementById('proximo_jogo').style.display = 'block';
